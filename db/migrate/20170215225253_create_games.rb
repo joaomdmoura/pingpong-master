53c55117d4ad3e36f2c18218b345a85347f2f343
Joao M. D. Moura <joaomdmoura@gmail.com>
2017-02-16 00:48:55 -0200

    migrating to postgres in order to deploy to heroku

--- db/migrate/20170215225253_create_games.rb
+++ db/migrate/20170215225253_create_games.rb
@@ -1,8 +1,8 @@
 class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
-      t.references :player, index: true, foreign_key: true
-      t.references :opponent, index: true, foreign_key: true
+      t.references :player, references: :users, index: true
+      t.references :opponent, references: :users, index: true
       t.date :played_at
       t.integer :player_score
       t.integer :opponent_score
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170216014411) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.integer  "player_id"
     t.integer  "opponent_id"
@@ -24,8 +27,8 @@ ActiveRecord::Schema.define(version: 20170216014411) do
     t.string   "result"
   end
 
-  add_index "games", ["opponent_id"], name: "index_games_on_opponent_id"
-  add_index "games", ["player_id"], name: "index_games_on_player_id"
+  add_index "games", ["opponent_id"], name: "index_games_on_opponent_id", using: :btree
+  add_index "games", ["player_id"], name: "index_games_on_player_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",     null: false
@@ -43,7 +46,7 @@ ActiveRecord::Schema.define(version: 20170216014411) do
     t.float    "rating",                 default: 1000.0
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
